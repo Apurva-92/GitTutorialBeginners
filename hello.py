1209cfa6a9149f6025e56c9bd048eb3e15679ccb
Apurva-92 <apurvavhatkar92@gmail.com>
2025-09-06 12:40:26 +0530

    Design completed

--- hello.py
+++ hello.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import numpy as np
+import sklearn
 
 def hello():
     print("Hello")
        
 hello()
+
+a = 10
+b = 5
+print(a+b)
+
+print("End")
+print("Thanks")
+print("End")
     
 
